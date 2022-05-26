8286f6fbbb207803b77c8710c88262628d037412
NW WEI <13936763538@163.com>
2022-05-26 15:46:15 +0800

    新增ABC类

---
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -0,0 +1,12 @@
+package com.atguigu.util;
+
+/**
+ * @author: 13936
+ * @date: 2022/5/26 15:44
+ * @description:
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
